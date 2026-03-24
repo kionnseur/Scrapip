ac8b163c81ed38b6ea167f0b6536c94fd83323ef
ycandaes <candaes.yan@gmail.com>
2026-03-24 10:47:39 +0100

    Fix: linux webview clamp at 50;50

--- src/main.rs
+++ src/main.rs
@@ -3,53 +3,36 @@ use tao::event_loop::{ControlFlow, EventLoop};
 use tao::window::WindowBuilder;
 use wry::WebViewBuilder;
 
+use crate::views::Views;
 use crate::views::builder::Builder;
 
 mod views;
 
 fn main() {
     let event_loop = EventLoop::new();
-    
     let window = WindowBuilder::new()
         .with_title("ScraPiP")
-        .with_inner_size(tao::dpi::LogicalSize::new(900.0, 650.0))
         .build(&event_loop)
         .unwrap();
 
     let mut view_builder = views::builder::ViewsBuilder::default();
+    
     let home_view = view_builder
-        .set_header("<header><h1>Header</h1></header>")
-        .set_body("<main><p>Home page of ScraPiP.</p></main>")
-        .set_footer("<footer><p>Footer</p></footer>")
+        .set_body("
+            <main>
+                <form>
+                    <label for=\"name\">Name:</label>
+                    <input type=\"text\" id=\"name\" name=\"name\">
+                    <button type=\"submit\">Submit</button>
+                </form>
+            </main>
+        ")
         .set_style("<style>body{animation:a 2s infinite alternate}@keyframes a{to{transform:scale(1.1)}}</style>")
         .set_script("<script>alert('Home page loaded');</script>")
         .build();
 
 
-    #[cfg(target_os = "linux")]
-    let _webview = {
-        use gtk::prelude::*;
-        use tao::platform::unix::WindowExtUnix;
-        use wry::WebViewBuilderExtUnix;
-
-        let vbox = window.default_vbox().unwrap();
-        let fixed = gtk::Fixed::new();
-        fixed.show_all();
-        vbox.pack_start(&fixed, true, true, 0);
-        
-        WebViewBuilder::new()
-            .with_html(&home_view)
-            .with_devtools(false)
-            .build_gtk(&fixed)
-            .unwrap()
-    };
-
-    #[cfg(not(target_os = "linux"))]
-    let _webview = WebViewBuilder::new()
-        .with_html(&home_view)
-        .with_devtools(false)
-        .build(&window)
-        .unwrap();
+    let _webview = generate_webview(&window, home_view);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = match event {
@@ -60,4 +43,36 @@ fn main() {
             _ => ControlFlow::Wait,
         };
     });
+}
+
+
+fn generate_webview(window: &tao::window::Window, html_content: Views) -> wry::WebView {
+   
+   #[cfg(target_os = "linux")]
+    let _webview = {
+        use gtk::prelude::*;
+        use tao::platform::unix::WindowExtUnix;
+        use wry::WebViewBuilderExtUnix;
+
+        let vbox = window.default_vbox().expect("Failed to get default vbox");
+
+        vbox.show_all();
+
+        let webview = WebViewBuilder::new()
+            .with_html(&html_content)
+            .with_devtools(false)
+            .build_gtk(vbox)
+            .expect("Failed to build WebView for Linux");
+
+        webview
+    };
+
+    #[cfg(not(target_os = "linux"))]
+    let _webview = WebViewBuilder::new()
+        .with_html(&html_content)
+        .with_devtools(false)
+        .build(&window)
+        .expect("Failed to build WebView for non-Linux platforms");
+
+    _webview
 }
